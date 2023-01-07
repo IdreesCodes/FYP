3f44ca8631cba12e476d8b00f27ffdb840b51b9d
Idrees <eesidr5@gmail.com>
2023-01-08 01:11:19 +0500

    added text_field

--- lib/view/login/login_screen.dart
+++ lib/view/login/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../res/components/input_text_field.dart';
 import '../../res/components/round_button.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -7,17 +8,33 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final emailController = TextEditingController();
+    final emailFocusNode = FocusNode();
+
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          RoundButton(
-            title: 'Login',
-            onPress: () {},
-            // loading: true,
-          )
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            InputTextField(
+                myController: emailController,
+                focusNode: emailFocusNode,
+                onFieldSubmittedValue: (value) {},
+                keyBoardType: TextInputType.emailAddress,
+                obscureText: false,
+                hint: 'Email',
+                onValidator: (value) {
+                  return value.isEmpty ? 'enter email' : null;
+                }),
+            RoundButton(
+              title: 'Login',
+              onPress: () {},
+              loading: true,
+            )
+          ],
+        ),
       ),
     );
   }
