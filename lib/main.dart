d95e73b1d823c2bd24923be503f70264f4f9d1f9
Idrees <eesidr5@gmail.com>
2023-01-09 00:18:34 +0500

    added utils and toast

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tech_media/res/color.dart';
 import 'package:tech_media/res/fonts.dart';
@@ -5,7 +6,9 @@ import 'package:tech_media/utils/routes/route_name.dart';
 import 'package:tech_media/utils/routes/routes.dart';
 import 'package:tech_media/view/splash/splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
--- lib/view/signup/sign_up_screen.dart
+++ lib/view/signup/sign_up_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:tech_media/utils/routes/route_name.dart';
+import 'package:tech_media/view_model/services/signup/signup_controller.dart';
 
 import '../../res/components/input_text_field.dart';
 import '../../res/components/round_button.dart';
+import '../../utils/utils.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({Key? key}) : super(key: key);
@@ -41,108 +44,143 @@ class _SignUpScreenState extends State<SignUpScreen> {
       ),
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: height * .05,
-                ),
-                Text(
-                  'Welcome To COMTACH',
-                  style: Theme.of(context).textTheme.headline3,
-                ),
-                SizedBox(
-                  height: height * .01,
-                ),
-                Text(
-                  'Enter Your Credentials To Create Account',
-                  style: Theme.of(context).textTheme.subtitle1,
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(
-                  height: height * .01,
-                ),
-                Form(
-                    key: _formKey,
-                    child: Padding(
-                      padding: EdgeInsets.only(
-                          top: height * .06, bottom: height * .01),
+            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+            child: ChangeNotifierProvider(
+                create: (_) => SignUpController(),
+                child: Consumer<SignUpController>(
+                  builder: (context, provider, child) {
+                    return SingleChildScrollView(
                       child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          InputTextField(
-                              myController: emailController,
-                              focusNode: emailFocusNode,
-                              onFieldSubmittedValue: (value) {},
-                              keyBoardType: TextInputType.emailAddress,
-                              obscureText: false,
-                              hint: 'Username',
-                              onValidator: (value) {
-                                return value.isEmpty ? 'enter username' : null;
-                              }),
                           SizedBox(
-                            height: height * 0.01,
+                            height: height * .05,
                           ),
-                          InputTextField(
-                              myController: emailController,
-                              focusNode: emailFocusNode,
-                              onFieldSubmittedValue: (value) {},
-                              keyBoardType: TextInputType.emailAddress,
-                              obscureText: false,
-                              hint: 'Email',
-                              onValidator: (value) {
-                                return value.isEmpty ? 'enter email' : null;
-                              }),
+                          Text(
+                            'Welcome To COMTACH',
+                            style: Theme.of(context).textTheme.headline3,
+                          ),
                           SizedBox(
-                            height: height * 0.01,
+                            height: height * .01,
                           ),
-                          InputTextField(
-                              myController: passwordController,
-                              focusNode: passwordFocusNode,
-                              onFieldSubmittedValue: (value) {},
-                              keyBoardType: TextInputType.emailAddress,
-                              obscureText: false,
-                              hint: 'Password',
-                              onValidator: (value) {
-                                return value.isEmpty ? 'enter password' : null;
-                              }),
+                          Text(
+                            'Enter Your Credentials To Create Account',
+                            style: Theme.of(context).textTheme.subtitle1,
+                            textAlign: TextAlign.center,
+                          ),
+                          SizedBox(
+                            height: height * .01,
+                          ),
+                          Form(
+                              key: _formKey,
+                              child: Padding(
+                                padding: EdgeInsets.only(
+                                    top: height * .06, bottom: height * .01),
+                                child: Column(
+                                  children: [
+                                    InputTextField(
+                                        myController: userNameController,
+                                        focusNode: userNameFocusNode,
+                                        onFieldSubmittedValue: (value) {},
+                                        keyBoardType:
+                                            TextInputType.emailAddress,
+                                        obscureText: false,
+                                        hint: 'Username',
+                                        onValidator: (value) {
+                                          return value.isEmpty
+                                              ? 'enter username'
+                                              : null;
+                                        }),
+                                    SizedBox(
+                                      height: height * 0.01,
+                                    ),
+                                    InputTextField(
+                                        myController: emailController,
+                                        focusNode: emailFocusNode,
+                                        onFieldSubmittedValue: (value) {
+                                          Utils.fieldFocus(
+                                              context,
+                                              emailFocusNode,
+                                              passwordFocusNode);
+                                        },
+                                        keyBoardType:
+                                            TextInputType.emailAddress,
+                                        obscureText: false,
+                                        hint: 'Email',
+                                        onValidator: (value) {
+                                          return value.isEmpty
+                                              ? 'enter email'
+                                              : null;
+                                        }),
+                                    SizedBox(
+                                      height: height * 0.01,
+                                    ),
+                                    InputTextField(
+                                        myController: passwordController,
+                                        focusNode: passwordFocusNode,
+                                        onFieldSubmittedValue: (value) {},
+                                        keyBoardType:
+                                            TextInputType.emailAddress,
+                                        obscureText: true,
+                                        hint: 'Password',
+                                        onValidator: (value) {
+                                          return value.isEmpty
+                                              ? 'enter password'
+                                              : null;
+                                        }),
+                                  ],
+                                ),
+                              )),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          RoundButton(
+                            loading: provider.loading,
+                            title: 'Sign Up',
+                            onPress: () {
+                              if (_formKey.currentState!.validate()) {
+                                provider.signup(
+                                  userNameController.text.toString(),
+                                  emailController.text.toString(),
+                                  passwordController.text.toString(),
+                                );
+                              }
+                            },
+                            //  loading: true,
+                          ),
+                          const SizedBox(
+                            height: 18,
+                          ),
+                          InkWell(
+                            onTap: () {
+                              Navigator.pushNamed(context, RouteName.loginView);
+                            },
+                            child: Text.rich(
+                              TextSpan(
+                                  text: 'Already have an account? ',
+                                  children: [
+                                    TextSpan(
+                                      text: 'Login',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .headline2!
+                                          .copyWith(
+                                              fontSize: 15,
+                                              decoration:
+                                                  TextDecoration.underline),
+                                    ),
+                                  ]),
+                            ),
+                          )
                         ],
                       ),
-                    )),
-                const SizedBox(
-                  height: 10,
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                RoundButton(
-                  title: 'Sign Up',
-                  onPress: () {},
-                  //  loading: true,
-                ),
-                const SizedBox(
-                  height: 18,
-                ),
-                InkWell(
-                  onTap: () {
-                    Navigator.pushNamed(context, RouteName.loginView);
+                    );
                   },
-                  child: Text.rich(
-                    TextSpan(text: 'Already have an account? ', children: [
-                      TextSpan(
-                        text: 'Login',
-                        style: Theme.of(context).textTheme.headline2!.copyWith(
-                            fontSize: 15, decoration: TextDecoration.underline),
-                      ),
-                    ]),
-                  ),
-                )
-              ],
-            ),
-          ),
-        ),
+                ))),
       ),
     );
   }
