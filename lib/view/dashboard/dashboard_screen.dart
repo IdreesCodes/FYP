4ffc8b13726ad111d501e1336c600577a41d3700
Idrees <eesidr5@gmail.com>
2023-01-12 03:08:17 +0500

    added homescreen

--- lib/view/dashboard/dashboard_screen.dart
+++ lib/view/dashboard/dashboard_screen.dart
@@ -2,7 +2,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 import 'package:tech_media/view/dashboard/profile.dart';
+import 'package:tech_media/view/dashboard/video_call.dart';
 
+import '../../home/homescreen.dart';
 import '../../res/color.dart';
 import '../../utils/routes/route_name.dart';
 import '../../view_model/services/session_controller.dart';
@@ -18,10 +20,10 @@ class _DashboardScreenState extends State<DashboardScreen> {
   final controller = PersistentTabController(initialIndex: 0);
   List<Widget> _buildScreen() {
     return [
-      const Text('Home'),
+      HomeScreen(),
       const Text('Chat'),
       const Text('Add'),
-      const Text('Message'),
+      VideoCall(),
       ProfileScreen(),
     ];
   }
